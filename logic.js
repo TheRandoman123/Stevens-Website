2e65b39b54532a1bc8d288462ceb421a2fbe788f
TheRandoman123 <nerfsteven@gmail.com>
2017-12-04 20:45:24 -0500

    update A.4.1
    added the start of encription app

--- logic.js
+++ logic.js
@@ -18,3 +18,19 @@ window.onclick = function(event) {
     }
   }
 }
+
+ 
+// INIT
+var myString   = "https://www.titanesmedellin.com/";
+var myPassword = "myPassword";
+
+
+// PROCESS
+var encrypted = CryptoJS.AES.encrypt(myString, myPassword);
+var decrypted = CryptoJS.AES.decrypt(encrypted, myPassword);
+document.getElementById("demo0").innerHTML = myString;
+document.getElementById("demo1").innerHTML = encrypted;
+document.getElementById("demo2").innerHTML = decrypted;
+document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+
+ 
